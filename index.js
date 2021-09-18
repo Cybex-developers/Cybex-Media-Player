332762f34d9955147119c5df762268699c05c00c
Cybex-developers <80897558+Cybex-developers@users.noreply.github.com>
2021-09-18 09:26:50 +0530

    commited
    Now you can launch the app without downloading!

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function GoHome(){window.location.href = "https://cybex-developers.github.io/Cyb
 function GoConPage(){window.location.href = "./Contact";}
 function GoDownload(){window.location.href = "./Cybex Media Player 2021.7.1 Setup.exe";}
 function ReadDocs(){window.location.href = "./Docs";}
+function Launch() {
+    app=window.open("./Cybex_Media_Player","Cybex Media Player","height=450,width=650");
+    if (window.focus) {app.focus()}
+    return false;
+}
