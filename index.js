9ff14fa7d6aa125a51d333b8be15549dd880e8c3
Cybex-Developers <Cybex-Developers@gmail.com>
2021-09-18 18:11:51 +0530

    comited

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ function Download(){
     GoDownload();
 }
 function GoHome(){window.location.href = "https://cybex-developers.github.io/Cybex-Media-Player/";}
-function GoConPage(){window.location.href = "./Contact";}
-function GoDownload(){window.location.href = "./Cybex Media Player 2021.7.1 Setup.exe";}
-function ReadDocs(){window.location.href = "./Docs";}
+function GoConPage(){window.location.href = "./ContactUs.html";}
+function GoAboutPage(){window.location.href = "./AboutUs.html";}
+function GoDownload(){window.location.href = "./Downloads.html";}
+function ReadDocs(){window.location.href = "./Docs.html";}
 function Launch() {
     app=window.open("./Cybex_Media_Player","Cybex Media Player","height=450,width=650");
     if (window.focus) {app.focus()}
