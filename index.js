40a3dca136c48bfb1bf58146f03e605c11e78cc3
Cybex-Developers <Cybex-Developers@gmail.com>
2021-09-24 14:22:25 +0530

    commited

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function GoHome(){window.location.href = "./";}
-function GoConPage(){window.location.href = "./ContactUs";}
+function GoConPage(){window.location.href = "./Help";}
 function GoAboutPage(){window.location.href = "./AboutUs";}
 function GoDownload(){window.location.href = "./Downloads";}
 function ReadDocs(){window.location.href = "./Docs";}
