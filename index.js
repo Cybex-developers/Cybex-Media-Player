3382b5b8c266678cd4a01087709e63c05ce429b7
Cybex-developers <Cybex.developers@gmail.com>
2021-12-23 13:43:41 +0530

    commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function GoAboutPage(){window.location.href = "./AboutUs";}
 function GoDownload(){window.location.href = "./Downloads";}
 function ReadDocs(){window.location.href = "./Docs";}
 function Download() {
-    window.open("./archives/2021/Cybex Media Player 2021.2.1 Setup.exe")
+    window.open("./archives/2021/Cybex Media Player 2021.3.1 Setup.exe")
 }
 function Launch() {
     app = window.open("./Cybex_Media_Player/index.html","Cybex Media Player","popup,start-maximized")
